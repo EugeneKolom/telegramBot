8f4f16b8f3b18ed6b887baffcb7ead198792f934
EugeneKolom <luxevgeniy@gmail.com>
2025-01-11 14:32:26 +0100

    Delete inviting.py imports from handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,12 +2,10 @@ from .base import router as base_router
 from .group_parsing import router as group_parsing_router
 from .group_management import router as group_management_router
 from .user_parsing import router as user_parsing_router
-from .inviting import router as inviting_router
 
 __all__ = [
     'base_router',
     'group_parsing_router',
     'group_management_router',
     'user_parsing_router',
-    'inviting_router'
 ]
